ce6c7fb9e6f7bf1d84e75cada80b63b2971a52ca
Luke Privett <privett1@umbc.edu>
2016-09-29 23:47:41 -0400

    added time, commented out trouble code

--- Clustering.py
+++ Clustering.py
@@ -1,24 +1,26 @@
 # python Clustering.py numberOfClusters file.txt\
 import sys
-
+import time
 
 def main():
+    start_time = time.time()
     # file input -> read the 2d points
     points = list()
-    with(sys.argv[1]) as file:
-        for line in file:
-            line = line.sp
-            lit(' ')
-            x = line[0]
-            y = line[1]
-            points.append(Point(x, y))
-    # display graph -> verify read correctly
+#    with(sys.argv[1]) as file:
+#        for line in file:
+#            line = line.sp
+#            lit(' ')
+#            x = line[0]
+#            y = line[1]
+#            points.append(Point(x, y))
+            # display graph -> verify read correctly
 
-    # cluster until optimal
-        # cluster forms k groups, each of which
-    # display clustered graph
-        # when the results don't change, it is done
-
+            # cluster until optimal
+            # cluster forms k groups, each of which
+            # display clustered graph
+            # when the results don't change, it is done
+    print ("End")
+    print ("Time taken: " + str(time.time() - start_time) + ' s')
 
 main()
 
